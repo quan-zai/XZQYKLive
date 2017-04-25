4956e61a434437ad3287b8b44774331071efab32
xuzhengquan <xuzhengquan@yundianjia.com>
2017-04-25 13:52:02 +0800

    Initial Commit

---
+++ XZQYKLive/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XZQYKLive
+//
+//  Created by 徐正权 on 2017/4/25.
+//  Copyright © 2017年 xuzhengquan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
